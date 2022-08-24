9ad7999a1a5be8e11ab341295075608bab247bd5
nick <nicolas.ai.nomis@gmail.com>
2022-08-24 14:19:21 +0900

    [feat] app.ts 내용 삭제 및 SQSClient 추가

--- src/app.ts
+++ src/app.ts
@@ -1,32 +1,3 @@
-import { SQSClient, ReceiveMessageCommand, DeleteMessageCommand } from '@aws-sdk/client-sqs';
-
-const client = new SQSClient({
-  region: 'ap-northeast-2',
-});
-
-const QueueUrl = process.env.QUEUE_URL;
-
-const getMessage = () =>
-  client.send(
-    new ReceiveMessageCommand({
-      QueueUrl,
-    })
-  );
-
-const deleteMessage = (ReceiptHandle: string) =>
-  client.send(
-    new DeleteMessageCommand({
-      QueueUrl,
-      ReceiptHandle,
-    })
-  );
-
-const start = async () => {
-  const { Messages } = await getMessage();
-  if (Messages == null) return;
-  const [{ Body, MessageId, ReceiptHandle }] = Messages;
-  console.log({ MessageId, Body });
-  if (ReceiptHandle) await deleteMessage(ReceiptHandle);
-};
+const start = async () => {};
 
 start();
