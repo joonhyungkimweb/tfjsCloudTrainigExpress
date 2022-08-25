2b1b2710296ca7ca417380ee49090c8f160f13d9
nick <nicolas.ai.nomis@gmail.com>
2022-08-25 15:52:53 +0900

    [feat] App ts 수정

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,34 @@
-const start = async () => {};
+import { loadAndProcessCSVData } from './Modules/DataProcessor';
+import { getTrainingParams } from './Modules/MessagePasers';
+import { LoadModel } from './Modules/ModelLoader';
+import { createModelSaver } from './Modules/ModelSaver';
+import { trainModel } from './Modules/ModelTrainer';
+
+const start = async () => {
+  const { params, clearMessage } = await getTrainingParams();
+  const trainingDataset = await loadAndProcessCSVData(
+    params.datasetPath,
+    params.xColumns,
+    params.yColumns
+  );
+  const model = await LoadModel(params.modelPath, params.weightsPath);
+  const result = await trainModel(
+    trainingDataset,
+    model,
+    {
+      optimizer: params.optimizer,
+      loss: params.loss,
+      metrics: params.metrics,
+    },
+    {
+      batchSize: params.batchSize,
+      epochs: params.epochs,
+      shuffle: params.shuffle,
+      validationSplit: params.validationSplit,
+    }
+  );
+  model.save(createModelSaver(params.userId, params.modelName));
+  clearMessage();
+};
 
 start();
