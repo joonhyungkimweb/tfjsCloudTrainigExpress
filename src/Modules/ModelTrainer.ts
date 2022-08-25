11bbf38653a179b74b02ba9a14aeafa3820a7ab6
nick <nicolas.ai.nomis@gmail.com>
2022-08-25 13:13:07 +0900

    [feat] ModelTrainer 추가

---
+++ src/Modules/ModelTrainer.ts
@@ -0,0 +1,17 @@
+import { LayersModel, ModelCompileArgs, ModelFitArgs, Tensor } from '@tensorflow/tfjs-node';
+
+export const trainModel = (
+  {
+    xs,
+    ys,
+  }: {
+    xs: Tensor | Tensor[];
+    ys: Tensor | Tensor[];
+  },
+  model: LayersModel,
+  compileConfig: ModelCompileArgs,
+  trainConfig: ModelFitArgs
+) => {
+  model.compile(compileConfig);
+  model.fit(xs, ys, trainConfig);
+};
