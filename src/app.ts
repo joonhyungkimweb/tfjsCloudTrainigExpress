99c39f59597b90f4108f23b3a4900e59f0d81872
nick <nicolas.ai.nomis@gmail.com>
2022-08-29 07:34:27 +0900

    [feat] 인스턴스 종료 요청 및 에러 처리 추가

--- src/app.ts
+++ src/app.ts
@@ -3,32 +3,40 @@ import { getTrainingParams } from './Modules/MessagePasers';
 import { LoadModel } from './Modules/ModelLoader';
 import { createModelSaver } from './Modules/ModelSaver';
 import { trainModel } from './Modules/ModelTrainer';
+import { terminateInstance } from './Modules/TeminatEC2';
 
 const start = async () => {
-  const { params, clearMessage } = await getTrainingParams();
-  const trainingDataset = await loadAndProcessCSVData(
-    params.datasetPath,
-    params.xColumns,
-    params.yColumns
-  );
-  const model = await LoadModel(params.modelPath, params.weightsPath);
-  const result = await trainModel(
-    trainingDataset,
-    model,
-    {
-      optimizer: params.optimizer,
-      loss: params.loss,
-      metrics: params.metrics,
-    },
-    {
-      batchSize: params.batchSize,
-      epochs: params.epochs,
-      shuffle: params.shuffle,
-      validationSplit: params.validationSplit,
-    }
-  );
-  model.save(createModelSaver(params.userId, params.modelName));
-  clearMessage();
+  try {
+    const { params, clearMessage } = await getTrainingParams();
+    const trainingDataset = await loadAndProcessCSVData(
+      params.datasetPath,
+      params.xColumns,
+      params.yColumns
+    );
+    const model = await LoadModel(params.modelPath, params.weightsPath);
+    const result = await trainModel(
+      trainingDataset,
+      model,
+      {
+        optimizer: params.optimizer,
+        loss: params.loss,
+        metrics: params.metrics,
+      },
+      {
+        batchSize: params.batchSize,
+        epochs: params.epochs,
+        shuffle: params.shuffle,
+        validationSplit: params.validationSplit,
+      }
+    );
+    model.save(createModelSaver(params.userId, params.modelName));
+    clearMessage();
+  } catch (err) {
+    console.error(err);
+    return;
+  } finally {
+    terminateInstance();
+  }
 };
 
 start();
