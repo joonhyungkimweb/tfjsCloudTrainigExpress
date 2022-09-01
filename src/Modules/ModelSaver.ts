efc4b4693dc04933048843684d9788046bd25770
nick <nicolas.ai.nomis@gmail.com>
2022-09-01 18:32:05 +0900

    [feat] ModelSaver 로컬 저장으로 수정

--- src/Modules/ModelSaver.ts
+++ src/Modules/ModelSaver.ts
@@ -1,6 +1,9 @@
 import { io } from '@tensorflow/tfjs-core';
+import fs from 'fs';
 
-export const createModelSaver = (modelName: string): io.IOHandler => ({
+const MODEL_DIR = process.env.MODEL_DIR || 'models';
+
+export const createModelSaver = (userId: string, modelName: string): io.IOHandler => ({
   save: async (modelArtifacts) => {
     const modelJSON = JSON.stringify({
       ...modelArtifacts,
@@ -12,8 +15,13 @@ export const createModelSaver = (modelName: string): io.IOHandler => ({
       ],
     });
 
+    fs.mkdirSync(`${MODEL_DIR}/${userId}/models/`, { recursive: true });
+
+    fs.writeFileSync(`${MODEL_DIR}/${userId}/models/${modelName}.json`, modelJSON);
+
     if (modelArtifacts.weightData != null) {
       const modelWeights = Buffer.from(modelArtifacts.weightData);
+      fs.writeFileSync(`${MODEL_DIR}/${userId}/models/${modelName}.weights.bin`, modelWeights);
     }
 
     return {
