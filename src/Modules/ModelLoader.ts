feeaa89b21954c55b6a61575cfa8edef32023d76
nick <nicolas.ai.nomis@gmail.com>
2022-09-01 18:31:29 +0900

    [feat] modelLoader 커스텀 로직으로 수정

--- src/Modules/ModelLoader.ts
+++ src/Modules/ModelLoader.ts
@@ -1,3 +1,17 @@
 import { loadLayersModel, io } from '@tensorflow/tfjs-node-gpu';
+import { io as coreIo } from '@tensorflow/tfjs-core';
 
-export const LoadModel = async (modelFile: Blob, weightsFile: Blob) => {};
+const convertBuffertToString = (buffer: Buffer) =>
+  String.fromCharCode(...Array.from(new Uint16Array(buffer)));
+
+export const LoadModel = async (modelBuffer: Buffer, weightBuffer: Buffer) => {
+  const convertedModelArtifacts: coreIo.ModelJSON = JSON.parse(convertBuffertToString(modelBuffer));
+
+  return await loadLayersModel({
+    load: () =>
+      io.getModelArtifactsForJSON(convertedModelArtifacts, async (weightsManifest) => [
+        weightsManifest.map(({ weights }) => weights).flat(),
+        weightBuffer.buffer,
+      ]),
+  });
+};
