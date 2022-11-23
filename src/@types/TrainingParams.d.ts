c8fdcd28c7278e97451fe352f1a5f64ef9443ee2
nick <nicolas.ai.nomis@gmail.com>
2022-11-23 20:36:04 +0900

    [feat] APICall parameterTypes 추가

--- src/@types/TrainingParams.d.ts
+++ src/@types/TrainingParams.d.ts
@@ -15,6 +15,27 @@ export interface TrainingParams {
   validationSplit: number;
 }
 
+export enum TrainingLossFunction {
+  categoricalCrossentropy,
+  sparseCategoricalCrossentropy,
+  meanSquaredError,
+  meanAbsoluteError,
+  binaryCrossentropy,
+}
+
+export enum TrainingMetrics {
+  accuracy,
+  mse,
+  precision,
+  crossentropy,
+}
+
+export enum TrainingOptimizer {
+  SGD,
+  ADAM,
+  ADAMAX,
+}
+
 export interface CSVParams extends TrainingParams {
   xColumns: number[];
   yColumns: number[];
@@ -24,4 +45,35 @@ export interface ImageParams extends TrainingParams {
   height: number;
   channel: number;
   normalize: boolean;
+}
+export interface TrainingParameters {
+  epochs: number;
+  batchSize: number;
+  learningRate: number;
+  loss: TrainingLossFunction;
+  metrics: TrainingMetrics;
+  optimizer: TrainingOptimizer;
+  shuffle: boolean;
+  trainingOptions: CSVParams | ImageParams;
+  datasetId: number;
+}
+
+export interface TrainingEpochParameters {
+  epochsDone: number;
+  history: {
+    loss: number;
+    valLoss: number;
+    subMetric: number;
+    valSubMetric: number;
+  };
+  modelFile: {
+    fileName: string;
+    filePath: string;
+    fileSize: number;
+  };
+  weightsFile?: {
+    fileName: string;
+    filePath: string;
+    fileSize: number;
+  };
 }
