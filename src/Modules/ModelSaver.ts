8b820263a7b9d64b67deb92d206b0d65037b0c44
nick <nicolas.ai.nomis@gmail.com>
2022-11-24 11:29:27 +0900

    [feat] modelSaver 수정

--- src/Modules/ModelSaver.ts
+++ src/Modules/ModelSaver.ts
@@ -1,10 +1,10 @@
 import { io } from '@tensorflow/tfjs-core';
 import { putObject } from './Storage';
+import path from 'path';
 
-export const createModelSaver = (prefix: string, modelFileName: string): io.IOHandler => ({
+export const createModelSaver = (modelPath: string, weightsPath: string): io.IOHandler => ({
   save: async (modelArtifacts) => {
-    const modelPath = `${prefix}/${modelFileName}.json`;
-    const weightsPath = `${prefix}/${modelFileName}.weights.bin`;
+    console.log(path.basename(weightsPath));
 
     await putObject(
       modelPath,
@@ -12,7 +12,7 @@ export const createModelSaver = (prefix: string, modelFileName: string): io.IOHa
         ...modelArtifacts,
         weightsManifest: [
           {
-            paths: [`./${modelFileName}.weights.bin`],
+            paths: [path.basename(weightsPath)],
             weights: modelArtifacts.weightSpecs,
           },
         ],
