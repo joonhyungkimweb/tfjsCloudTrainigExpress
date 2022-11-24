e7a22e5f56c33433786d8c1af7c708e97226ed7e
nick <nicolas.ai.nomis@gmail.com>
2022-11-24 11:13:48 +0900

    [feat] loadAndProcessCSVData 수정

--- src/Modules/DataProcessor.ts
+++ src/Modules/DataProcessor.ts
@@ -1,6 +1,9 @@
 import { parseCsvData, UnheadedCSV } from './CSVPaser';
 import { getObject } from './Storage';
 import { tensor, util } from '@tensorflow/tfjs-node-gpu';
+import { streamToJSONObject } from './streamConverter';
+import { Readable } from 'stream';
+import { CSVMetaData } from '../@types/TrainingParams';
 
 const loadCSVDataset = async (datasetURL: string) => {
   const {
@@ -14,11 +17,15 @@ const extractColumns = (dataSet: UnheadedCSV[], columns: number[]) =>
   dataSet.map((data) => data.filter((_, index) => isSelectedColumn(columns, index)));
 
 export const loadAndProcessCSVData = async (
-  datasetURL: string,
+  metaDataURL: string,
   xColumns: number[],
   yColumns: number[]
 ) => {
-  const data = await loadCSVDataset(datasetURL);
+  const { Body: metaDataBody } = await getObject(metaDataURL);
+  const {
+    fileData: { filepath },
+  } = (await streamToJSONObject(metaDataBody as Readable)) as CSVMetaData;
+  const data = await loadCSVDataset(filepath);
   util.shuffle(data);
   const xsArray = extractColumns(data, xColumns);
   const ysArray = extractColumns(data, yColumns);
